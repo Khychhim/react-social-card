825ee5f1e998f1654594e803c8f42fc459ccd624
Lim <khychhim_lim@dc01.fujixerox.net>
2018-05-11 15:53:09 +1200

    css class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SocialCard from './village/SocialCard';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
+      <div>
+        <SocialCard />
       </div>
+
     );
   }
 }
--- src/village/SocialCard.js
+++ src/village/SocialCard.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style/socialcard.css';
 
 class SocialCard extends React.Component{
   render() {
     return (
-      <div />
+      <div className="frame">
+        <div class="Title"/>
+        <div class="Body"></div>
+        <div class="Footer"></div>
+      </div>
+
     );
   }
 }
